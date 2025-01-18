9317bc7a235f34a3772ae0042dd28c99e2d68123
young-min-ko <young.min.ko4321@gmail.com>
2025-01-18 10:29:35 -0500

    added asset from AD
    added the mushroom sprite from AD under tower defense

--- tilemap-prototype/Base/path_2d.gd
+++ tilemap-prototype/Base/path_2d.gd
@@ -2,11 +2,9 @@ extends Path2D
 
 @onready var path_follow = $PathFollow2D
 @onready var ene_animation = $PathFollow2D/EnemySpirte/body
-@onready var ene_clo_animation = $PathFollow2D/EnemySpirte/body/clothes
-@onready var ene_hair_animation = $PathFollow2D/EnemySpirte/body/hair
-@onready var ene_hat_animation = $PathFollow2D/EnemySpirte/body/hat
 var ene_speed = 100
 var last_fram_pos = Vector2()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,24 +14,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-
 	path_follow.progress += ene_speed * delta
-	print(last_fram_pos, path_follow.position, last_fram_pos - path_follow.position)
 	var diff_vector = path_follow.position - last_fram_pos
 	if diff_vector.x != 0 :
 		ene_animation.play("right")
-		ene_clo_animation.play("right")
-		ene_hair_animation.play("right")
-		ene_hat_animation.play("right")
+	
 	elif diff_vector.y != 0 :
 		if diff_vector.y > 0 :
-			ene_animation.play("bot")
-			ene_clo_animation.play("bot")
-			ene_hair_animation.play("bot")
-			ene_hat_animation.play("bot")
+			ene_animation.play("right")
+		
 		elif diff_vector.y < 0 :
-			ene_animation.play("top")
-			ene_clo_animation.play("top")
-			ene_hair_animation.play("top")
-			ene_hat_animation.play("top")
+			ene_animation.play("right")
+
 	last_fram_pos = path_follow.position
