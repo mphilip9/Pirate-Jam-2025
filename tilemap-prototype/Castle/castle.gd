561d9e941ff4f286ad80f1283ad0501ac7bc0166
young-min-ko <young.min.ko4321@gmail.com>
2025-01-18 22:28:00 -0500

    castle placement and path adjustment for castle
    - added new scene castle with muschroom castle asset
    - added castle.gd for health and health label
    - adjusted end_x to be lower in _ready for base.gd to give some space for the mushroom castle and make the path shorter
    - adjusted _ready for base.gd to set y axis for the castle each time it generates new path so that the castle would be at the end of the path all the time (ln 22)

---
+++ tilemap-prototype/Castle/castle.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var max_health: int = 5
+
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		label.text = str(current_health) + "/" + str(max_health)
+		var red: Color = Color.RED
+		var white: Color = Color.WHITE
+		label.modulate = red.lerp(white,float(current_health) / float(max_health))
+		if current_health < 1:
+			get_tree().reload_current_scene()
+@onready var label: Label = $Label
+# Called when the node enters the scene tree for the first time.
+
+func _ready() -> void:
+	current_health = max_health
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func take_damage() :
+	current_health -= 1
