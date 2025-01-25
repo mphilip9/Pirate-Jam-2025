73754216674bebf287f5a75f9485afc4b6f189de
young-min-ko <young.min.ko4321@gmail.com>
2025-01-25 18:26:37 -0500

    Hand Tower added
    - hand tower spawns a single target hand underneath the enemy that stops them for its duration
    - added FingerSpikes.png for attack animation
    - hand_tower_stats.tres, hand_tower.tscn and hand_projectile.tscn was added
    - hand_projectile.gd was modeled after seismic_projectile.gd
       - the difference is that it tracks if it hit the first target with var first_target_hit
       - if first_target_hit is false hit the enemy me first_traget_hit = true
       - if first_target_hit is true it does not affect other bodies entered

---
+++ tilemap-prototype/Tower/TowerAttacks/hand_projectile.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+@export var speed: float = 300.0
+@export var damage: int
+# The enemy to fire towards
+var target
+var curr_position: Vector2
+var first_target_hit: bool = false
+signal projectile_hit()
+
+func _ready():
+	animated_sprite_2d.play('default')
+
+func _physics_process(delta):
+	# disappear at the end of frame progress
+	if animated_sprite_2d.frame_progress == 1 :
+		queue_free()
+		return	
+	# set position to the target and stay in that position
+	if curr_position == Vector2() :
+		curr_position = target.global_position
+	global_position = curr_position
+	
+func _on_hitbox_body_entered(body):
+	if first_target_hit == false :
+		body.get_parent().take_damage(damage)
+		body.get_parent().crowd_control_slow(200, 1)
+		first_target_hit = true
+	else :
+		return
+	
