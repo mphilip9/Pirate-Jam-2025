ac2f4b9b7cfcfe8fcaa28e7926793f1f9c547f99
young-min-ko <young.min.ko4321@gmail.com>
2025-01-25 00:08:38 -0500

    fixed seismic_projectile.gd logic
    - the projectile was playing when _ready() and playing again when it was inside hitbox of a body entered so deleted playing animation when a enemy body was entered.
    - increased the AoE of seismic_projectile

--- tilemap-prototype/Tower/TowerAttacks/seismic_projectile.gd
+++ tilemap-prototype/Tower/TowerAttacks/seismic_projectile.gd
@@ -13,19 +13,15 @@ func _ready():
 	animated_sprite_2d.play('default')
 
 func _physics_process(delta):
+	# disappear at the end of frame progress
 	if animated_sprite_2d.frame_progress == 1 :
 		queue_free()
 		return	
+	# set position to the target and stay in that position
 	if curr_position == Vector2() :
 		curr_position = target.global_position
 	global_position = curr_position
 	
-
-
-
 func _on_hitbox_body_entered(body):
-#	The parent Node has the take_damage function, not the body itself
-# We could add a check to see if the body is in the enemy group
-	animated_sprite_2d.play('default')
 	body.get_parent().take_damage(damage)
 	
